424cfa4f3bcf16c89cd8d86b952e78c477ff5584
root <kirthan@myria.net>
2024-08-07 23:46:37 -0400

    fixed typo in Home.js that was causing the navbar links to not work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -81,8 +81,8 @@ const Home = () => {
     <div>
       <Checklogin/>
       <ul>
-        <li><Link href="/profile">Profile</Link></li>
-        <li><Link href="/viewchats">Chats</Link></li>
+        <li><Link href={"/profile"}>Profile</Link></li>
+        <li><Link href={"/viewchats"}>Chats</Link></li>
         <li><Link onClick={logout}>Logout</Link></li>
       </ul>
       <form className="searchbar" onSubmit={handleSubmit}>
